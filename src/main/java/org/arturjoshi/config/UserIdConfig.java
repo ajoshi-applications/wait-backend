0d350a30733f92d0a964f47271d972b7f8f5a9c5
Artur <arturjoshi.mailbox@gmail.com>
2016-05-25 10:09:58 +0300

    Hiding events methods

--- src/main/java/org/arturjoshi/config/UserIdConfig.java
+++ src/main/java/org/arturjoshi/config/UserIdConfig.java
@@ -1,5 +1,6 @@
 package org.arturjoshi.config;
 
+import org.arturjoshi.events.domain.Event;
 import org.arturjoshi.users.domain.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -11,5 +12,6 @@ public class UserIdConfig extends RepositoryRestMvcConfiguration {
     @Override
     protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(User.class);
+        config.exposeIdsFor(Event.class);
     }
 }
--- src/main/java/org/arturjoshi/events/repository/EventsRepository.java
+++ src/main/java/org/arturjoshi/events/repository/EventsRepository.java
@@ -3,7 +3,32 @@ package org.arturjoshi.events.repository;
 import org.arturjoshi.events.domain.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface EventsRepository extends CrudRepository<Event, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    Event save(Event e);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Event event);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    Event findOne(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Event> findAll();
 }
